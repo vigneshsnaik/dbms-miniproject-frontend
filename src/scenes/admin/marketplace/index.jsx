f9b5f92eca84a9a7477dc2a574def878dd6841b6
Mekha <mekhalnair@gmail.com>
2023-11-16 22:16:38 +0530

    changing  marketplace

--- src/scenes/admin/marketplace/index.jsx
+++ src/scenes/admin/marketplace/index.jsx
@@ -7,16 +7,17 @@ import LibraryAddIcon from "@mui/icons-material/LibraryAdd";
 
 import Header from "../../../components/Header";
 import { mockMarketplacedata } from "../../../data/mockData";
+
 function Marketplace() {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
+
   return (
     <Box m="20px">
       <Box display="flex" justifyContent="space-between" alignItems="center">
         <Header
           title="MARKETPLACE"
-          subtitle="Welcome to our marketplace! Here you can find a variety of products and
-        services offered by our vendors."
+          subtitle="Welcome to our marketplace! Here you can find a variety of products and services offered by our vendors."
         />
       </Box>
       <Box
@@ -29,14 +30,19 @@ function Marketplace() {
         {mockMarketplacedata.map((element, i) => (
           <Box
             key={`${element.id}-${i}`}
-            gridColumn="span 3"
+            gridColumn={{ xs: "span 12", sm: "span 6", md: "span 4", lg: "span 3" }}
             gridRow="span 2"
             backgroundColor={colors.primary[400]}
             display="flex"
-            alignItems="center"
-            justifyContent="center"
+            flexDirection="column"
+            justifyContent={"flex-end"}
+            padding={"0.3rem"}
           >
-            <Box>
+            <Box
+              display="flex"
+              justifyContent="space-between"
+              margin="3px"
+            >
               <Typography
                 color={colors.greenAccent[500]}
                 variant="h5"
@@ -44,7 +50,10 @@ function Marketplace() {
               >
                 {element.name}
               </Typography>
-              <Typography color={colors.grey[100]}>
+              <Typography
+                color={colors.grey[100]}
+                textTransform="capitalize"
+              >
                 {element.assetType}
               </Typography>
             </Box>
@@ -54,15 +63,36 @@ function Marketplace() {
                 {element.descrption}
               </Typography>
             </Box>
-            <Box color={colors.grey[100]}>{element.dateacquired}</Box>
+  
+    <Box display={{md:"flex",xl:"flex"}}
+      justifyContent={{md:"space-between"}}
+      alignItems={{md:"center"}}
+      marginTop="5px"
+    >
+         <Box
+              backgroundColor="gray"
+              width={{ xs: "100%", md: "90px" }}
+              borderRadius="4px"
+              textAlign="center"
+              height="30px"
+              p="5px 10px"
+               marginBottom={{xs:"5px"}}
+            >
+              {element.dateacquired}
+            </Box>
+
             <Box
               backgroundColor={colors.greenAccent[500]}
               p="5px 10px"
+              width={{ xs: "100%", md: "60px" }}
               borderRadius="4px"
+              textAlign="center"
             >
               ₹ {element.price}
             </Box>
-          </Box>
+    
+            </Box>
+            </Box>
         ))}
       </Box>
     </Box>
--- src/theme.js
+++ src/theme.js
@@ -1,6 +1,5 @@
 import { createContext, useState, useMemo } from "react";
 import { createTheme } from "@mui/material/styles";
-
 // color design tokens export
 export const tokens = (mode) => ({
   ...(mode === "dark"
