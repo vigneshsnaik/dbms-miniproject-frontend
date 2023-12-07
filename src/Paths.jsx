fc6aa7373bf57b9215938354e5f5202f621f3a21
Naik Ji <vigneshsnaik03@gmail..com>
2023-12-07 13:18:34 +0530

    delete unwanted path

--- src/Paths.jsx
+++ src/Paths.jsx
@@ -6,12 +6,6 @@ import AdminManageAssets from "./scenes/admin/manageassets";
 import AdminForm from "./scenes/admin/form";
 import AdminFAQ from "./scenes/admin/faq";
 import AdminMarketplace from "./scenes/admin/marketplace";
-import UserDashboard from "./scenes/user/dashboard";
-import UserTransactions from "./scenes/user/transactions";
-import UserManageAssets from "./scenes/user/manageassets";
-import UserForm from "./scenes/user/form";
-import UserFAQ from "./scenes/user/faq";
-import UserMarketplace from "./scenes/user/marketplace";
 import { Routes, Route } from "react-router-dom";
 import UserLogin from "./scenes/global/UserLogin";
 import SignUp from "./scenes/global/Login";
@@ -19,23 +13,16 @@ import SignUp from "./scenes/global/Login";
 export default function Paths() {
   return (
     <Routes>
-      <Route path="/" element={<SignUp />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/login" element={<UserLogin />} />
       {/* Admin Paths */}
-      <Route path="/admin/" element={<AdminDashboard />} />
-      <Route path="/admin/manageUsers" element={<AdminUsers />} />
-      <Route path="/admin/assets" element={<AdminManageAssets />} />
-      <Route path="/admin/transactions" element={<AdminTransactions />} />
-      <Route path="/admin/form" element={<AdminForm />} />
-      <Route path="/admin/faq" element={<AdminFAQ />} />
-      <Route path="/admin/marketplace" element={<AdminMarketplace />} />
-      {/* User Paths */}
-      <Route path="/user/" element={<UserDashboard />} />
-      <Route path="/user/userlogin" element={<UserLogin />} />
-      <Route path="/user/transactions" element={<UserTransactions />} />
-      <Route path="/user/assets" element={<UserManageAssets />} />
-      <Route path="/user/faq" element={<UserFAQ />} />
-      <Route path="/user/form" element={<UserForm />} />
-      <Route path="/user/marketplace" element={<UserMarketplace />} />
+      <Route path="/" element={<AdminDashboard />} />
+      <Route path="/manageUsers" element={<AdminUsers />} />
+      <Route path="/assets" element={<AdminManageAssets />} />
+      <Route path="/transactions" element={<AdminTransactions />} />
+      <Route path="/form" element={<AdminForm />} />
+      <Route path="/faq" element={<AdminFAQ />} />
+      <Route path="/marketplace" element={<AdminMarketplace />} />
     </Routes>
   );
 }
