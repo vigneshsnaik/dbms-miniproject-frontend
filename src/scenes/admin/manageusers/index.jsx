bf02fd4fa8f4535305cb6a13b4b14532a4cbc8da
Naik Ji <vigneshsnaik03@gmail..com>
2023-12-07 13:17:09 +0530

    add user list integration

--- src/scenes/admin/manageusers/index.jsx
+++ src/scenes/admin/manageusers/index.jsx
@@ -1,45 +1,69 @@
 import { Box, Typography, useTheme } from "@mui/material";
-import { DataGrid } from "@mui/x-data-grid";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 import { tokens } from "../../../theme";
-import { mockDataTeam } from "../../../data/mockData";
 import AdminPanelSettingsOutlinedIcon from "@mui/icons-material/AdminPanelSettingsOutlined";
 import LockOpenOutlinedIcon from "@mui/icons-material/LockOpenOutlined";
-import SecurityOutlinedIcon from "@mui/icons-material/SecurityOutlined";
 import Header from "../../../components/Header";
+import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+import { Button } from "@mui/material";
+const supabase = createClient(
+  process.env.REACT_APP_SUPABASE_URL,
+  process.env.REACT_APP_SUPABASE_KEY
+);
 
 const Users = () => {
+  const [owners, setOwners] = useState([]);
+
+  const [userData, setUserData] = useState(null);
+  const [isAdmin, setIsAdmin] = useState(false);
+  useEffect(() => {
+    // Check if user data exists in cache
+    const cachedUserData = localStorage.getItem("userId");
+    if (cachedUserData) {
+      setUserData(cachedUserData);
+    }
+
+    if (localStorage.getItem("type") === true) {
+      setIsAdmin(true);
+      getOwners();
+    }
+  }, []);
+
+  async function getOwners() {
+    const { data } = await supabase.from("login").select();
+    setOwners(data);
+    console.log(data);
+  }
+
+  async function deleteOwner(id) {
+    await supabase.from("owner").delete().eq("id", id);
+    getOwners();
+  }
+
+  const handleDelete = (params) => {
+    const { id } = params.row;
+    deleteOwner(id);
+  };
+
+  const handleEdit = (params) => {
+    console.log("Edit clicked for ID:", params.row.id);
+  };
+
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   const columns = [
-    { field: "id", headerName: "ID" },
+    { field: "id", headerName: "ID", flex: 2 },
     {
       field: "name",
       headerName: "Name",
       flex: 1,
-      cellClassName: "name-column--cell",
     },
     {
-      field: "age",
-      headerName: "Age",
-      type: "number",
-      headerAlign: "left",
-      align: "left",
-    },
-    {
-      field: "phone",
-      headerName: "Phone Number",
+      field: "userType",
+      headerName: "User Type",
       flex: 1,
-    },
-    {
-      field: "email",
-      headerName: "Email",
-      flex: 1,
-    },
-    {
-      field: "accessLevel",
-      headerName: "Access Level",
-      flex: 1,
-      renderCell: ({ row: { access } }) => {
+      renderCell: ({ row: { admin } }) => {
         return (
           <Box
             width="60%"
@@ -48,20 +72,43 @@ const Users = () => {
             display="flex"
             justifyContent="center"
             backgroundColor={
-              access === "admin"
-                ? colors.greenAccent[600]
-                : access === "manager"
-                ? colors.greenAccent[700]
-                : colors.greenAccent[700]
+              admin ? colors.greenAccent[600] : colors.greenAccent[700]
             }
             borderRadius="4px"
           >
-            {access === "admin" && <AdminPanelSettingsOutlinedIcon />}
-            {access === "manager" && <SecurityOutlinedIcon />}
-            {access === "user" && <LockOpenOutlinedIcon />}
+            {admin ? (
+              <AdminPanelSettingsOutlinedIcon />
+            ) : (
+              <LockOpenOutlinedIcon />
+            )}
             <Typography color={colors.grey[100]} sx={{ ml: "5px" }}>
-              {access}
+              {admin ? "Admin" : "Owner"}
             </Typography>
+          </Box>
+        );
+      },
+    },
+    {
+      field: "actions",
+      headerName: "Actions",
+      flex: 1,
+      renderCell: (params) => {
+        return (
+          <Box>
+            <Button
+              variant="contained"
+              color="secondary"
+              onClick={() => handleDelete(params)}
+            >
+              Delete
+            </Button>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => handleEdit(params)}
+            >
+              Edit
+            </Button>
           </Box>
         );
       },
@@ -69,40 +116,54 @@ const Users = () => {
   ];
 
   return (
-    <Box m="20px">
-      <Header title="TEAM" subtitle="Managing the Team Members" />
-      <Box
-        m="40px 0 0 0"
-        height="75vh"
-        sx={{
-          "& .MuiDataGrid-root": {
-            border: "none",
-          },
-          "& .MuiDataGrid-cell": {
-            borderBottom: "none",
-          },
-          "& .name-column--cell": {
-            color: colors.greenAccent[300],
-          },
-          "& .MuiDataGrid-columnHeaders": {
-            backgroundColor: colors.blueAccent[700],
-            borderBottom: "none",
-          },
-          "& .MuiDataGrid-virtualScroller": {
-            backgroundColor: colors.primary[400],
-          },
-          "& .MuiDataGrid-footerContainer": {
-            borderTop: "none",
-            backgroundColor: colors.blueAccent[700],
-          },
-          "& .MuiCheckbox-root": {
-            color: `${colors.greenAccent[200]} !important`,
-          },
-        }}
-      >
-        <DataGrid checkboxSelection rows={mockDataTeam} columns={columns} />
+    <>
+      <Box m="20px">
+        <Header title="Users" subtitle="Managing the Users" />
+        <Box
+          m="40px 0 0 0"
+          height="75vh"
+          sx={{
+            "& .MuiDataGrid-root": {
+              border: "none",
+            },
+            "& .MuiDataGrid-cell": {
+              borderBottom: "none",
+            },
+            "& .name-column--cell": {
+              color: colors.greenAccent[300],
+            },
+            "& .MuiDataGrid-columnHeaders": {
+              backgroundColor: colors.blueAccent[700],
+              borderBottom: "none",
+            },
+            "& .MuiDataGrid-virtualScroller": {
+              backgroundColor: colors.primary[400],
+            },
+            "& .MuiDataGrid-footerContainer": {
+              borderTop: "none",
+              backgroundColor: colors.blueAccent[700],
+            },
+            "& .MuiCheckbox-root": {
+              color: `${colors.greenAccent[200]} !important`,
+            },
+          }}
+        >
+          {!isAdmin ? (
+            <Typography variant="h3" color={colors.grey[100]}>
+              You are not authorized to view this page
+            </Typography>
+          ) : (
+            <DataGrid
+              rows={owners}
+              columns={columns}
+              components={{
+                Toolbar: GridToolbar,
+              }}
+            />
+          )}
+        </Box>
       </Box>
-    </Box>
+    </>
   );
 };
 
